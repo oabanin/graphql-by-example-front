9f8bd1a453a0e82397c996bd18abe36975aae9f8
Oleh Abanin <110968635+olehoutvio@users.noreply.github.com>
2023-02-22 11:44:08 +0200

    Update examples.ts

--- examples.ts
+++ examples.ts
@@ -24,6 +24,32 @@ export const RETURN_REASON_SET_UPDATE_MANY_MUTATION = gql(String.raw`
   }
 `);
 
+export const RETURN_REASON_SET_UPDATE_MANY_MUTATION_TEST = gql(`#graphql
+    mutation ReturnReasonSetUpdateManyMutation(
+    $reasonSets: [ReasonSetInput!]!
+    $setsToRemove: [String!]!
+    $reasonsToRemove: [String!]!
+  ) {
+    ReturnReasonSet_DeleteMany(ids: $setsToRemove)
+    ReturnReason_DeleteMany(ids: $reasonsToRemove)
+
+    ReturnReasonSet_GetByBrand: ReturnReasonSet_UpdateMany(reasonSets: $reasonSets) {
+      id
+      name
+      reasons {
+        id
+        labels {
+          language
+          body
+        }
+        requiresAttachingPhoto
+        requiresAddingDescription
+        shippingCostAlwaysFree
+      }
+    }
+  }
+`);
+
 
 
 export const RETURN_REASON_SET_UPDATE_MANY_MUTATION = gql`
@@ -42,3 +68,29 @@ export const RETURN_REASON_SET_UPDATE_MANY_MUTATION = gql`
 
   ${RETURN_REASON_SET_FRAGMENT}
 `;
+
+gql(`#graphql
+        mutation ReturnReasonSetUpdateManyMutation(
+            $reasonSets: [ReasonSetInput!]!
+            ${setsToRemove.length > 0 ? '$setsToRemove: [String!]!' : ''}
+            ${reasonsToRemove.length > 0 ? '$reasonsToRemove: [String!]!' : ''}
+        ) {
+            ${setsToRemove.length > 0 ? 'ReturnReasonSet_DeleteMany(ids: $setsToRemove)' : ''}
+            ${reasonsToRemove.length > 0 ? 'ReturnReason_DeleteMany(ids: $reasonsToRemove)' : ''}
+
+            ReturnReasonSet_GetByBrand: ReturnReasonSet_UpdateMany(reasonSets: $reasonSets) {
+                id
+                name
+                reasons {
+                    id
+                    labels {
+                        language
+                        body
+                    }
+                    requiresAttachingPhoto
+                    requiresAddingDescription
+                    shippingCostAlwaysFree
+                }
+            }
+        }
+        `)
