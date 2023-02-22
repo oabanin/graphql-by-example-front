4a38733bdf0ae73e165c6f219a40efaabac23fda
Oleh Abanin <110968635+olehoutvio@users.noreply.github.com>
2023-02-22 11:22:48 +0200

    Create examples.ts

---
+++ examples.ts
@@ -0,0 +1,25 @@
+export const RETURN_REASON_SET_UPDATE_MANY_MUTATION = gql(String.raw`
+    mutation ReturnReasonSetUpdateManyMutation(
+    $reasonSets: [ReasonSetInput!]!
+    $setsToRemove: [String!]!
+    $reasonsToRemove: [String!]!
+  ) {
+    ReturnReasonSet_DeleteMany(ids: $setsToRemove)
+    ReturnReason_DeleteMany(ids: $reasonsToRemove)
+
+    ReturnReasonSet_GetByBrand: ReturnReasonSet_UpdateMany(reasonSets: $reasonSets) {
+      id
+      name
+      reasons {
+        id
+        labels {
+          language
+          body
+        }
+        requiresAttachingPhoto
+        requiresAddingDescription
+        shippingCostAlwaysFree
+      }
+    }
+  }
+`);
